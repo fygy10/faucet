72d160078bcc4de2567a167b701ed0b85712f289
fygy10 <niko.timothyl@gmail.com>
2022-04-23 09:35:51 -0400

    How much javascript I could write in 15 minutes for a working game.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # faucet
-How much javascript I could do in 15 minutes
+How much javascript I could do in 15 minutes and I tried to make a simple game.
+It is mostly alerts, a loop, prompts, and an array.
---
+++ external.js
@@ -0,0 +1,16 @@
+const temps = ["hot" , "cold"];
+const men = 'cold';
+const women = 'hot';
+
+function faucet () {
+    for (i = 0; i = 2; i++) {
+    let faucetTemp = prompt("What is the water temperature? Hot or cold?");
+if (faucetTemp.toLowerCase() == 'hot') {
+    alert("Turn the cold water on then!")
+}
+else { (faucetTemp.toLowerCase = 'null', "", 'cold')
+    alert("Sorry, the hot water faucet is broken; you are stuck with cold water.") }
+}
+}
+
+faucet ();
